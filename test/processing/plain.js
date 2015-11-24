367de62cd9fb813d44f04a12a33e8f62fca20646
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-11-24 18:05:31 +0100

    [FIX] Conceal of array of objects

--- test/processing/plain.js
+++ test/processing/plain.js
@@ -234,3 +234,225 @@ describe( "Processing: Plain Object (with complex)", function() {
 		personInstance.should.have.property( "parent" ).that.is.a( "string" ).and.equals( "parent-id" );
 	} );
 } );
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+describe( "Processing: Array of Plain Object", function() {
+	it( "should omit null values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType );
+
+		var personInstance = PersonType.construct( "id", null, "apiKey", "special" );
+		var typeInfo       = new TypeInfo( "person", personType );
+		var helper         = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "name" );
+
+		helper.omitNull( [ personInstance ] );
+
+		personInstance.should.not.have.property( "name" );
+	} );
+
+	it( "should omit hidden values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.HIDDEN );
+
+		var personInstance = PersonType.construct( "id", "name", "apiKey", "special" );
+		var typeInfo       = new TypeInfo( "person", personType );
+		var helper         = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" );
+
+		helper.omitHidden( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.not.have.property( "apiKey" );
+	} );
+
+	it( "should omit read-only values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.READ_ONLY );
+
+		var personInstance = PersonType.construct( "id", "name", "apiKey", "special" );
+		var typeInfo       = new TypeInfo( "person", personType );
+		var helper         = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" );
+
+		helper.omitReadOnly( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.not.have.property( "apiKey" );
+	} );
+
+	it( "should conceal concealed values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.CONCEALED );
+
+		var personInstance = PersonType.construct( "id", "name", "apiKey", "special" );
+		var typeInfo       = new TypeInfo( "person", personType );
+		var helper         = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "apiKey" );
+
+		helper.conceal( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( true );
+	} );
+
+	it( "should conceal concealed values with given replacement", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.CONCEALED );
+
+		var personInstance = PersonType.construct( "id", "name", "apiKey", "special" );
+		var typeInfo       = new TypeInfo( "person", personType );
+		var helper         = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "apiKey" );
+
+		helper.conceal( [ personInstance ], TypeInfo.USERCLASS_USER, false, "foo" );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "foo" );
+	} );
+} );
+
+describe( "Processing: Array of Plain Object (with complex)", function() {
+	it( "should omit null values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType );
+
+		var personInstance    = PersonType.construct( "id", null, "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "id", null, "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "name" );
+		personInstance.parent.should.have.property( "name" );
+
+		helper.omitNull( [ personInstance ] );
+
+		personInstance.should.not.have.property( "name" );
+		personInstance.parent.should.not.have.property( "name" );
+	} );
+
+	it( "should omit hidden values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.HIDDEN );
+
+		var personInstance    = PersonType.construct( "id", "name", "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "id", "name", "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" );
+		personInstance.parent.should.have.property( "apiKey" );
+
+		helper.omitHidden( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.not.have.property( "apiKey" );
+		personInstance.parent.should.not.have.property( "apiKey" );
+	} );
+
+	it( "should omit read-only values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.READ_ONLY );
+
+		var personInstance    = PersonType.construct( "id", "name", "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "id", "name", "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" );
+		personInstance.parent.should.have.property( "apiKey" );
+
+		helper.omitReadOnly( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.not.have.property( "apiKey" );
+		personInstance.parent.should.not.have.property( "apiKey" );
+	} );
+
+	it( "should conceal concealed values", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.CONCEALED );
+
+		var personInstance    = PersonType.construct( "id", "name", "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "id", "name", "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "apiKey" );
+		personInstance.parent.should.have.property( "apiKey" ).that.equals( "apiKey" );
+
+		helper.conceal( [ personInstance ], TypeInfo.USERCLASS_USER );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( true );
+		personInstance.parent.should.have.property( "apiKey" ).that.equals( true );
+	} );
+
+	it( "should conceal concealed values with given replacement", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.CONCEALED );
+
+		var personInstance    = PersonType.construct( "id", "name", "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "id", "name", "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "apiKey" );
+		personInstance.parent.should.have.property( "apiKey" ).that.equals( "apiKey" );
+
+		helper.conceal( [ personInstance ], TypeInfo.USERCLASS_USER, false, "foo" );
+
+		personInstance.should.have.property( "apiKey" ).that.equals( "foo" );
+		personInstance.parent.should.have.property( "apiKey" ).that.equals( "foo" );
+	} );
+
+	it( "should reduce complex properties", function() {
+		var personType = PersonType.get();
+
+		new TypeDecorator( personType )
+			.decorate( "apiKey", TypeInfo.USERCLASS_USER, TypeInfo.CONCEALED );
+
+		var personInstance    = PersonType.construct( "id", "name", "apiKey", "special" );
+		personInstance.parent = PersonType.construct( "parent-id", "name", "apiKey", "special" );
+
+		var typeInfo = new TypeInfo( "person", personType );
+		typeInfo.markComplex( "parent", "person" );
+
+		var helper = new TypeHelper( typeInfo );
+		helper.reduceComplex( [ personInstance  ]);
+
+		personInstance.should.have.property( "parent" ).that.is.a( "string" ).and.equals( "parent-id" );
+	} );
+} );
